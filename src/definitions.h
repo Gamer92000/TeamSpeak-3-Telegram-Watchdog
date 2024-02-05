f9ed0adc07bcc5570623181e79c4b5fb8a9b9cca
Julian Imhof <julianimhof00@gmail.com>
2024-02-05 04:47:40 +0100

    :bookmark:: set proper semver

--- src/definitions.h
+++ src/definitions.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define PLUGIN_NAME "Telegram Watchdog"
-#define PLUGIN_VERSION "1.2.0"
+#define PLUGIN_VERSION "1.2.1"
 #define PLUGIN_AUTHOR "Gamer92000"
 #define PLUGIN_DESCRIPTION "This plugin adds a channel watchdog and Telegram notifications.";
 #define UPDATE_URL "https://julianimhof.de/files/TelegramWatchdogversion"
